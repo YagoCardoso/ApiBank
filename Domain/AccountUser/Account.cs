a75d330b392b9616538b1546677ad52d7b11481e
Yago Cardoso <48061155+YagoCardoso@users.noreply.github.com>
2024-04-02 20:26:19 -0300

    upgrade .net7 for .net8 and add repositories project and Domain project

---
+++ Domain/AccountUser/Account.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bank123.models
+{
+    public class Account
+    {
+        public string? NameClient {get; set;}
+        public string? Credit {get;set;}
+        public string? Balance {get;set;}
+
+
+    }
+}
